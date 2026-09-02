185347868d1062e237014685e66674d99dc67b67
mts0629 <soratobi93@gmail.com>
2026-09-02 23:58:55 +0900

    Get an encoder value

--- timer.ino
+++ timer.ino
@@ -15,6 +15,22 @@ const int duration_sec = 30;
 void setup() {
   M5.begin();
 
+  // Initialize I2C connection to
+  // Hat Mini Encoder
+  Wire.begin(0, 26, 100000UL);
+  delay(10);
+  Wire.beginTransmission(0x42);
+  Wire.endTransmission(true);
+  delay(100);
+
+  // Reset an encoder value
+  Wire.beginTransmission(0x42);
+  Wire.write(0x00);
+  for (int i = 0; i < 4; i++) {
+    Wire.write(0x0);
+  }
+  Wire.endTransmission(true);
+
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setTextColor(WHITE, BLACK);
   M5.Lcd.setRotation(0);
@@ -52,11 +68,24 @@ void loop() {
     M5.Lcd.setTextSize(5);
     M5.Lcd.printf("%d.%d", count / 10, count % 10);
   } else {
+    // Get an encoder value
+    Wire.beginTransmission(0x42);
+    Wire.write(0x00);
+    Wire.endTransmission(true);
+    Wire.requestFrom(0x42, 4);
+    uint8_t data[4];
+    for (int i = 0; i < 4; i++) {
+      data[i] = Wire.read();
+    }
+
     M5.Lcd.setTextSize(5);
     M5.Lcd.printf("%d s\n", duration_sec);
     M5.Lcd.setTextSize(3);
-    M5.Lcd.print("[A]\nstart");
-    
+    M5.Lcd.print("[A]\nstart\n");
+    uint32_t val = ((data[3] << 24) | (data[2] << 16) | (data[1] << 8) | data[0]);
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.printf("[Value]\n%10u", val);
+
     if (M5.BtnA.isPressed()) {
       if (state == STATE_STOP) {      
         beep(100);
