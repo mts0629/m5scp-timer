9cb4dd9a13504fd5fa17224803845b299ee2ce67
mts0629 <soratobi93@gmail.com>
2026-09-03 23:59:33 +0900

    Change duration

--- timer.ino
+++ timer.ino
@@ -10,26 +10,24 @@ State state;
 
 // Count in 100 ms
 int count;
-const int duration_sec = 30;
+int duration_sec = 0;
 
 void setup() {
   M5.begin();
 
-  // Initialize I2C connection to
-  // Hat Mini Encoder
+  // Initialize I2C connection to Hat Mini Encoder
   Wire.begin(0, 26, 100000UL);
   delay(10);
   Wire.beginTransmission(0x42);
   Wire.endTransmission(true);
   delay(100);
 
-  // Reset an encoder value
+  // Reset a counter
   Wire.beginTransmission(0x42);
-  Wire.write(0x00);
-  for (int i = 0; i < 4; i++) {
-    Wire.write(0x0);
-  }
+  Wire.write(0x40);
+  Wire.write(1);
   Wire.endTransmission(true);
+  delay(100);
 
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setTextColor(WHITE, BLACK);
@@ -66,31 +64,44 @@ void loop() {
 
   if (state == STATE_RUNNING) {
     M5.Lcd.setTextSize(5);
-    M5.Lcd.printf("%d.%d", count / 10, count % 10);
+    M5.Lcd.printf("%02d s\n%d", count / 10, count % 10);
   } else {
-    // Get an encoder value
     Wire.beginTransmission(0x42);
-    Wire.write(0x00);
+    Wire.write(0x10);
     Wire.endTransmission(true);
     Wire.requestFrom(0x42, 4);
     uint8_t data[4];
     for (int i = 0; i < 4; i++) {
       data[i] = Wire.read();
     }
+  
+    int val = ((data[3] << 24) | (data[2] << 16) | (data[1] << 8) | data[0]);
 
+    // Change duration
+    if (val > 0) {
+      duration_sec++;
+    } else if (val < 0) {
+      duration_sec--;
+    }
+    // Rotate for 60 sec
+    if (duration_sec > 59) {
+      duration_sec = 0;
+    } else if (duration_sec < 0) {
+      duration_sec = 59;
+    }
+    
     M5.Lcd.setTextSize(5);
-    M5.Lcd.printf("%d s\n", duration_sec);
+    M5.Lcd.printf("%02d s\n", duration_sec);
     M5.Lcd.setTextSize(3);
     M5.Lcd.print("[A]\nstart\n");
-    uint32_t val = ((data[3] << 24) | (data[2] << 16) | (data[1] << 8) | data[0]);
-    M5.Lcd.setTextSize(2);
-    M5.Lcd.printf("[Value]\n%10u", val);
+    M5.Lcd.print("[Knob]\nsec\n");
 
     if (M5.BtnA.isPressed()) {
       if (state == STATE_STOP) {      
         beep(100);
 
         switch_state();
+        return;
       }
     }
   }
@@ -99,11 +110,14 @@ void loop() {
 
   if (state == STATE_RUNNING) {
     count--;
+    if (count < 0) {
+      count = 0;
+    }
 
     if (count == 0) {
       // Print "0.0"
       M5.Lcd.setCursor(0, 0);
-      M5.Lcd.printf("%d.%d", count / 10, count % 10);
+      M5.Lcd.printf("%02d s\n%d", count / 10, count % 10);
 
       // Beep twice
       for (int i = 0; i < 2; i++) {
