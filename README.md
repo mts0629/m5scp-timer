8dd462b849860913936ecac296fcf5c9f90b3814
mts0629 <soratobi93@gmail.com>
2026-09-23 23:41:43 +0900

    Refactor

--- README.md
+++ README.md
@@ -4,6 +4,34 @@ Timer with M5Stick CPlus
 
 - [M5StickC Plus](https://docs.m5stack.com/ja/core/m5stickc_plus)
 
+## Usage
+
+When the device is power-on, it starts from the configuration state.
+
+### Configuration state
+
+- Button A
+    - Push
+        - When min/sec is selected: count up selected duration
+        - Otherwise: start timer -> [Running state]
+    - Hold
+        - When min/sec is selected: count up
+- Button B
+    - Push
+        - Select target duration: none -> min -> sec -> none -> min -> ...
+    - Hold
+        - When min/sec is selected: reset selected duration
+        - Otherwise: reset all duration
+
+### Running state
+
+- Button A: stop timer -> [Stop state]
+
+### Stop state
+
+- Button A: restart timer -> [Running state]
+- Button B: reset -> [Configuration state]
+
 ## License
 
 Unlicense
--- src/timer.ino
+++ src/timer.ino
@@ -2,17 +2,16 @@
 #include <M5Unified.h>
 
 // Timer state
-enum State {
-  STATE_CONFIG,
-  STATE_RUNNING,
-  STATE_STOP
+enum class State {
+    Config,
+    Running,
+    Stop
 };
-
 static State state;
 
 // Count in ms
 static int count;
-#define SCALE_SEC 1000
+constexpr int SCALE_SEC = 1000;
 
 // Start of elapsed count
 static unsigned long prev_ms;
@@ -21,26 +20,30 @@ static int sum_elapsed_ms;
 
 // Duration
 static int duration;
-#define MAX_DURATION ((99 * 60 + 59) * SCALE_SEC)
+constexpr int MAX_DURATION = ((99 * 60 + 59) * SCALE_SEC);
 
 // Button state
-#define BUTTON_NONE 0
-#define BUTTON_PRESSED 1
-#define BUTTON_HOLDING 2
+enum class ButtonState {
+    None,
+    Pressed,
+    Holding
+};
 
 // Duration selector
-#define SELECT_NONE 0
-#define SELECT_MIN 1
-#define SELECT_SEC 2
-static uint8_t selector;
+enum class Selector {
+    None,
+    Min,
+    Sec
+};
+static Selector selector;
 
 // Configuration saved address on EEPROM
-#define EEPROM_SAVE_ADDR 0
+constexpr int EEPROM_SAVE_ADDR = 0;
 
 // Time (min/sec)
 struct DispTime {
-  int min;
-  int sec;
+    int min;
+    int sec;
 };
 
 // Displayed time
@@ -49,348 +52,350 @@ static DispTime disp_time = { 0, 0 };
 static DispTime cfg_time = { 0, 0 };
 
 int cvt_to_duration(const DispTime cfg) {
-  return ((cfg.min * 60) + cfg.sec) * SCALE_SEC;
+    return ((cfg.min * 60) + cfg.sec) * SCALE_SEC;
 }
 
 void setup() {
-  auto cfg = M5.config();
-  M5.begin(cfg);
+    auto cfg = M5.config();
+    M5.begin(cfg);
 
-  // Initialization
-  M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setTextColor(WHITE, BLACK);
-  M5.Lcd.setRotation(0);
-  state = STATE_CONFIG;
-  selector = SELECT_NONE;
+    // Initialization
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setTextColor(WHITE, BLACK);
+    M5.Lcd.setRotation(0);
+    state = State::Config;
+    selector = Selector::None;
 
-  // Load saved configuration
-  EEPROM.begin(sizeof(DispTime));
-  EEPROM.get(EEPROM_SAVE_ADDR, cfg_time);
-  duration = cvt_to_duration(cfg_time);
+    // Load saved configuration
+    EEPROM.begin(sizeof(DispTime));
+    EEPROM.get(EEPROM_SAVE_ADDR, cfg_time);
+    duration = cvt_to_duration(cfg_time);
 }
 
 void switch_state(const State next_state) {
-  switch (next_state) {
-    case STATE_RUNNING:
-      if (state == STATE_CONFIG) {
-        count = duration;
-        disp_time = cfg_time;
-        sum_elapsed_ms = 0;
-      }
+    switch (next_state) {
+        case State::Running:
+            if (state == State::Config) {
+                count = duration;
+                disp_time = cfg_time;
+                sum_elapsed_ms = 0;
+            }
 
-      prev_ms = millis();
-      state = STATE_RUNNING;
-      break;
-    case STATE_STOP:
-      state = STATE_STOP;
-      break;
-    default: // STATE_CONFIG
-      state = STATE_CONFIG;
-      break;
-  }
+            prev_ms = millis();
+            state = State::Running;
+            break;
+        case State::Stop:
+            state = State::Stop;
+            break;
+        default: // State::Config
+            state = State::Config;
+            break;
+    }
 
-  M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.fillScreen(BLACK);
 }
 
 void print_time() {
-  int fg_color = WHITE;
-  // Change fg color
-  if (state != STATE_CONFIG) {
-    if (count <= (5 * SCALE_SEC)) {
-      // Remaining 5 sec: print by red
-      fg_color = RED;
-    } else if (count <= (10 * SCALE_SEC)) {
-      // Remaining 10 sec: print by yellow
-      fg_color = YELLOW;
+    int fg_color = WHITE;
+
+    // Change fg color
+    if (state != State::Config) {
+        if (count <= (5 * SCALE_SEC)) {
+            // Remaining 5 sec: print by red
+            fg_color = RED;
+        } else if (count <= (10 * SCALE_SEC)) {
+            // Remaining 10 sec: print by yellow
+            fg_color = YELLOW;
+        }
     }
-  }
  
-  M5.Lcd.setCursor(0, 0);
-  M5.Lcd.setTextColor(fg_color, BLACK);
-  
-  DispTime *t = (state == STATE_CONFIG) ? &cfg_time : &disp_time;
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.setTextColor(fg_color, BLACK);
+    
+    DispTime *t = (state == State::Config) ? &cfg_time : &disp_time;
 
-  // Minutes
-  M5.Lcd.setTextSize(10);
-  if ((state == STATE_CONFIG) && (selector == SELECT_MIN)) {
-    // Selected on configuration
-    M5.Lcd.setTextColor(YELLOW, BLACK);
-  }
-  M5.Lcd.printf("%02d", t->min);
-  M5.Lcd.setTextSize(2);
-  M5.Lcd.setTextColor(fg_color, BLACK);
-  M5.Lcd.print("'");
+    // Minutes
+    M5.Lcd.setTextSize(10);
+    if ((state == State::Config) && (selector == Selector::Min)) {
+        // Selected on configuration
+        M5.Lcd.setTextColor(YELLOW, BLACK);
+    }
+    M5.Lcd.printf("%02d", t->min);
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.setTextColor(fg_color, BLACK);
+    M5.Lcd.print("'");
 
-  // Seconds
-  M5.Lcd.setTextSize(10);
-  if ((state == STATE_CONFIG) && (selector == SELECT_SEC)) {
-    // Selected on configuration
-    M5.Lcd.setTextColor(YELLOW, BLACK);
-  }
-  M5.Lcd.printf("\n%02d", t->sec);
-  M5.Lcd.setTextSize(2);
-  M5.Lcd.setTextColor(fg_color, BLACK);
-  M5.Lcd.print("\"");
+    // Seconds
+    M5.Lcd.setTextSize(10);
+    if ((state == State::Config) && (selector == Selector::Sec)) {
+        // Selected on configuration
+        M5.Lcd.setTextColor(YELLOW, BLACK);
+    }
+    M5.Lcd.printf("\n%02d", t->sec);
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.setTextColor(fg_color, BLACK);
+    M5.Lcd.print("\"");
 }
 
 void print_state() {
-  M5.Lcd.setTextSize(10);
-  M5.Lcd.println();
-  M5.Lcd.setTextSize(3);
-  if (state == STATE_RUNNING) {
-    M5.Lcd.print("\nRUNNING");
-  } else if (state == STATE_STOP) {
-    M5.Lcd.print("\nSTOP");
-  } else {
-    M5.Lcd.print("\nCONFIG");
-  }
+    M5.Lcd.setTextSize(10);
+    M5.Lcd.println();
+    M5.Lcd.setTextSize(3);
+    if (state == State::Running) {
+        M5.Lcd.print("\nRUNNING");
+    } else if (state == State::Stop) {
+        M5.Lcd.print("\nSTOP");
+    } else {
+        M5.Lcd.print("\nCONFIG");
+    }
 }
 
 void change_duration(const int inc_val) {
-  int *selected = (selector == SELECT_SEC) ? &(cfg_time.sec) : &(cfg_time.min);
-  if (inc_val > 0) {
-    (*selected)++;
-  } else if (inc_val < 0) {
-    (*selected)--;
-  }
+    int *selected = (selector == Selector::Sec) ? &(cfg_time.sec)
+                                                : &(cfg_time.min);
+    if (inc_val > 0) {
+        (*selected)++;
+    } else if (inc_val < 0) {
+        (*selected)--;
+    }
 
-  // Rotate seconds
-  if (cfg_time.sec > 59) {
-    cfg_time.sec = 0;
-  } else if (cfg_time.sec < 0) {
-    cfg_time.sec = 59;
-  }
-  // Rotate minutes
-  if (cfg_time.min > 99) {
-    cfg_time.min = 0;
-  } else if (cfg_time.min < 0) {
-    cfg_time.min = 99;
-  }
+    // Rotate seconds
+    if (cfg_time.sec > 59) {
+        cfg_time.sec = 0;
+    } else if (cfg_time.sec < 0) {
+        cfg_time.sec = 59;
+    }
+    // Rotate minutes
+    if (cfg_time.min > 99) {
+        cfg_time.min = 0;
+    } else if (cfg_time.min < 0) {
+        cfg_time.min = 99;
+    }
 
-  duration = cvt_to_duration(cfg_time);
+    duration = cvt_to_duration(cfg_time);
 }
 
 void reset_config() {
-  cfg_time.min = 0;
-  cfg_time.sec = 0;
+    cfg_time.min = 0;
+    cfg_time.sec = 0;
 }
 
 void beep(const int time) {
-  constexpr int ms = 100;
-  constexpr int duration = 100;
-  constexpr int interval = 50;
+    constexpr int MS = 100;
+    constexpr int DURATION = 100;
+    constexpr int INTERVAL = 50;
 
-  for (int i = 0; i < time; i++) {
-    M5.Speaker.tone(2000, ms, 0, false);
-    delay(duration + interval);
-  }
+    for (int i = 0; i < time; i++) {
+        M5.Speaker.tone(2000, MS, 0, false);
+        delay(DURATION + INTERVAL);
+    }
 }
 
 void proc_time(const unsigned long start_ms, const unsigned long end_ms) {
-  int elapsed = (int)(end_ms - start_ms);
-  count -= elapsed;
-  sum_elapsed_ms += elapsed;
+    int elapsed = (int)(end_ms - start_ms);
+    count -= elapsed;
+    sum_elapsed_ms += elapsed;
 
-  if (sum_elapsed_ms >= SCALE_SEC) {
-    disp_time.sec--;
+    if (sum_elapsed_ms >= SCALE_SEC) {
+        disp_time.sec--;
 
-    if (disp_time.sec < 0) {
-      disp_time.sec = 59;
-      disp_time.min--;
+        if (disp_time.sec < 0) {
+            disp_time.sec = 59;
+            disp_time.min--;
+        }
+        if (disp_time.min < 0) {
+            disp_time.min = 0;
+        }
+
+        sum_elapsed_ms = (sum_elapsed_ms - SCALE_SEC);
     }
-    if (disp_time.min < 0) {
-      disp_time.min = 0;
-    }
-
-    sum_elapsed_ms = (sum_elapsed_ms - SCALE_SEC);
-  }
 }
 
-void finish_timer() {  
-  count = 0;
+void finish_timer() {    
+    count = 0;
 
-  // Print zero
-  print_time();
-  print_state();
+    // Print zero
+    print_time();
+    print_state();
 
-  beep(2);
+    beep(2);
 
-  // Wait 2 sec (wait 300 ms on beep x2)
-  delay(1700);
+    // Wait 2 sec (wait 300 ms on beep x2)
+    delay(1700);
 }
 
 bool is_btn_a_pressed() {
-  static bool prev_pressed = false;
-  static bool pressed = false;
+    static bool prev_pressed = false;
+    static bool pressed = false;
 
-  pressed = M5.BtnA.isPressed();
+    pressed = M5.BtnA.isPressed();
 
-  // Detect the change: (released -> pressed)
-  bool detected = (!prev_pressed && pressed);
-  prev_pressed = pressed;
+    // Detect the change: (released -> pressed)
+    bool detected = (!prev_pressed && pressed);
+    prev_pressed = pressed;
 
-  return detected;
+    return detected;
 }
 
-int get_btn_a_state() {
-  constexpr int hold_duration = SCALE_SEC / 2; // 500 ms
-  static bool pressed = false;
-  static unsigned long btn_a_press_start = 0;
+ButtonState get_btn_a_state() {
+    constexpr int HOLD_DURATION = SCALE_SEC / 2; // 500 ms
+    static bool pressed = false;
+    static unsigned long btn_a_press_start = 0;
 
-  unsigned long now;
-  if (M5.BtnA.isPressed()) {
-    if (!pressed) {
-      btn_a_press_start = millis();
-      pressed = true;
+    unsigned long now;
+    if (M5.BtnA.isPressed()) {
+        if (!pressed) {
+            btn_a_press_start = millis();
+            pressed = true;
+        }
+
+        now = millis();
+
+        if ((now - btn_a_press_start) > HOLD_DURATION) {
+            return ButtonState::Holding;
+        }
+    } else {
+        if (pressed) {
+            pressed = false;
+            return ButtonState::Pressed;
+        }
     }
 
-    now = millis();
-
-    if ((now - btn_a_press_start) > hold_duration) {
-      return BUTTON_HOLDING;
-    }
-  } else {
-    if (pressed) {
-      pressed = false;
-      return BUTTON_PRESSED;
-    }
-  }
-
-  return BUTTON_NONE;
+    return ButtonState::None;
 }
 
-int get_btn_b_state() {
-  constexpr int hold_duration = SCALE_SEC / 2; // 500 ms
-  static bool pressed = false;
-  static unsigned long btn_b_press_start = 0;
+ButtonState get_btn_b_state() {
+    constexpr int HOLD_DURATION = SCALE_SEC / 2; // 500 ms
+    static bool pressed = false;
+    static unsigned long btn_b_press_start = 0;
 
-  unsigned long now;
-  if (M5.BtnB.isPressed()) {
-    if (!pressed) {
-      btn_b_press_start = millis();
-      pressed = true;
+    unsigned long now;
+    if (M5.BtnB.isPressed()) {
+        if (!pressed) {
+            btn_b_press_start = millis();
+            pressed = true;
+        }
+
+        now = millis();
+
+        if ((now - btn_b_press_start) > HOLD_DURATION) {
+            return ButtonState::Holding;
+        }
+    } else {
+        if (pressed) {
+            pressed = false;
+            return ButtonState::Pressed;
+        }
     }
 
-    now = millis();
-
-    if ((now - btn_b_press_start) > hold_duration) {
-      return BUTTON_HOLDING;
-    }
-  } else {
-    if (pressed) {
-      pressed = false;
-      return BUTTON_PRESSED;
-    }
-  }
-
-  return BUTTON_NONE;
+    return ButtonState::None;
 }
 
 void run() {
-  unsigned long now_ms = millis();
-  proc_time(prev_ms, now_ms);
+    unsigned long now_ms = millis();
+    proc_time(prev_ms, now_ms);
 
-  print_time();
-  print_state();
+    print_time();
+    print_state();
 
-  if (is_btn_a_pressed()) {
-    beep(1);
-    switch_state(STATE_STOP);
-    return;
-  }
+    if (is_btn_a_pressed()) {
+        beep(1);
+        switch_state(State::Stop);
+        return;
+    }
 
-  if (count <= 0) {
-    finish_timer();
-    switch_state(STATE_CONFIG);
-  }
+    if (count <= 0) {
+        finish_timer();
+        switch_state(State::Config);
+    }
 
-  prev_ms = now_ms;
+    prev_ms = now_ms;
 }
 
 void stop() {
-  print_time();
-  print_state();
+    print_time();
+    print_state();
 
-  if (is_btn_a_pressed()) {
-    beep(1);
-    switch_state(STATE_RUNNING);
-    return;
-  }
+    if (is_btn_a_pressed()) {
+        beep(1);
+        switch_state(State::Running);
+        return;
+    }
 
-  if (get_btn_b_state() == BUTTON_PRESSED) {
-    beep(2);
-    switch_state(STATE_CONFIG);
-    return;
-  }
+    if (get_btn_b_state() == ButtonState::Pressed) {
+        beep(2);
+        switch_state(State::Config);
+        return;
+    }
 }
 
 void save_config() {
-  EEPROM.put(EEPROM_SAVE_ADDR, cfg_time);
-  EEPROM.commit();
+    EEPROM.put(EEPROM_SAVE_ADDR, cfg_time);
+    EEPROM.commit();
 }
 
 void configure() {
-  static bool btn_b_holding = false;
+    static bool btn_b_holding = false;
 
-  print_time();
-  print_state();
+    print_time();
+    print_state();
 
-  if (get_btn_a_state() == BUTTON_PRESSED) {
-    if (selector == SELECT_NONE) {
-      beep(1);
-      switch_state(STATE_RUNNING);
-      return;
-    } else {
-      change_duration(1);
+    if (get_btn_a_state() == ButtonState::Pressed) {
+        if (selector == Selector::None) {
+            beep(1);
+            switch_state(State::Running);
+            return;
+        } else {
+            change_duration(1);
+        }
+    } else if (get_btn_a_state() == ButtonState::Holding) {
+        if (selector != Selector::None) {
+            change_duration(1);
+        }
     }
-  } else if (get_btn_a_state() == BUTTON_HOLDING) {
-    if (selector != SELECT_NONE) {
-      change_duration(1);
+
+    if (get_btn_b_state() == ButtonState::Pressed) {
+        if (!btn_b_holding) {
+            beep(1);
+
+            selector = (selector == Selector::None) ? Selector::Min :
+                       (selector == Selector::Min) ? Selector::Sec :
+                       Selector::None;
+
+            if (selector == Selector::None) {
+                save_config();
+            }
+        }
+    } else if (get_btn_b_state() == ButtonState::Holding) {
+        if (!btn_b_holding) {
+            beep(2);
+
+            if (selector == Selector::Min) {
+                cfg_time.min = 0;
+            } else if (selector == Selector::Sec) {
+                cfg_time.sec = 0;
+            } else {
+                reset_config();
+            }
+
+            save_config();
+            btn_b_holding = true;
+        }
+    } else { // ButtonState::NONE
+        btn_b_holding = false;
     }
-  }
-
-  if (get_btn_b_state() == BUTTON_PRESSED) {
-    if (!btn_b_holding) {
-      beep(1);
-
-      selector = (selector == SELECT_NONE) ? SELECT_MIN :
-                 (selector == SELECT_MIN) ? SELECT_SEC :
-                 SELECT_NONE;
-
-      if (selector == SELECT_NONE) {
-        save_config();
-      }
-    }
-  } else if (get_btn_b_state() == BUTTON_HOLDING) {
-    if (!btn_b_holding) {
-      beep(2);
-
-      if (selector == SELECT_MIN) {
-        cfg_time.min = 0;
-      } else if (selector == SELECT_SEC) {
-        cfg_time.sec = 0;
-      } else {
-        reset_config();
-      }
-
-      save_config();
-      btn_b_holding = true;
-    }
-  } else { // BUTTON_NONE
-    btn_b_holding = false;
-  }
 }
 
 void loop() {
-  M5.update();
+    M5.update();
 
-  if (state == STATE_RUNNING) {
-    run();
-  } else if (state == STATE_STOP) {
-    stop();
-  } else { // STATE_CONFIG
-    configure();
-  }
+    if (state == State::Running) {
+        run();
+    } else if (state == State::Stop) {
+        stop();
+    } else { // State::Config
+        configure();
+    }
 
-  delay(100);
+    delay(100);
 }
